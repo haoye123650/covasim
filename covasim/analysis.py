850d20e165578fba3567f2b4dec6800c186a4ce9
haoye123650 <haoye123650@gmail.com>
2021-08-16 06:13:28 +0800

    Update analysis.py

--- covasim/analysis.py
+++ covasim/analysis.py
@@ -133,7 +133,7 @@ def validate_recorded_dates(sim, requested_dates, recorded_dates, die=True):
             print(errormsg)
     return
 
-class snapshotAdapter(snapshot(Analyzer), daily_stats(Analyzer)):
+class Snapshotadapter(snapshot(Analyzer), daily_stats(Analyzer)):
     '''
     this adapter is designed for get a snapshot of daily_stats.
     
